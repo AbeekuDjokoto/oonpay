f1272b378befa2714641b4fc252166f67d41617b
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2023-12-25 09:59:53 +0000

    fixed

--- src/features/website/components/banner/banner.jsx
+++ src/features/website/components/banner/banner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./banner.module.scss";
-import blackApple from "../../../../assets/icons/appleblack.svg";
-import googleBlack from "../../../../assets/icons/googleblack.svg";
+import whiteGoogle from "../../../../assets/icons/whitegoogleapp.svg";
+import appApple from "../../../../assets/icons/applewhite.svg";
 
 export default function Banner() {
   return (
@@ -10,18 +10,18 @@ export default function Banner() {
         <h1 className="text-3xl max-sm:text-[28px] text-white mx-auto tracking-[-1.1px] font-medium leading-[110%] text-center max-w-[600px] w-full">
           Do business with Oonpay
         </h1>
-        <div className="flex justify-center gap-2">
+        <div className="flex gap-3">
           <img
-            src={blackApple}
+            src={whiteGoogle}
             alt=""
             srcset=""
-            className="max-w-[135px] w-full h-10"
+            className="max-w-[135px] w-full"
           />
           <img
-            src={googleBlack}
+            src={appApple}
             alt=""
             srcset=""
-            className="max-w-[135px] w-full h-10"
+            className="max-w-[135px] w-full"
           />
         </div>
       </div>
--- src/features/website/components/footer/footer.jsx
+++ src/features/website/components/footer/footer.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import styles from "./footer.module.scss";
-import whiteGoogle from "../../../../assets/icons/whitegoogleapp.svg";
-import appApple from "../../../../assets/icons/applewhite.svg";
 import { AnimatedSection } from "../AnimateSection";
 import logo from "../../../../assets/icons/logo.png";
 
@@ -9,7 +7,7 @@ export default function Footer() {
   return (
     <nav className={styles.container}>
       <AnimatedSection>
-        <div className="max-w-[1440px] w-[90%] mx-auto justify-between grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-6">
+        <div className="max-w-[1440px] w-[90%] mx-auto justify-between grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-6">
           <img src={logo} alt="Oonpay logo" className="max-w-[50px] w-full" />
           <div className="flex flex-col gap-4">
             <h1 className="text-sm font-medium leading-[160%] w-full text-white">
@@ -78,25 +76,6 @@ export default function Footer() {
               <p className="text-sm leading-[160%] w-full text-white">
                 Our Story
               </p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-sm font-medium leading-[160%] w-full text-white">
-              Get the app
-            </h1>
-            <div className="flex flex-col gap-3">
-              <img
-                src={whiteGoogle}
-                alt=""
-                srcset=""
-                className="max-w-[135px] w-full"
-              />
-              <img
-                src={appApple}
-                alt=""
-                srcset=""
-                className="max-w-[135px] w-full"
-              />
             </div>
           </div>
         </div>
